b8e61bfdaa95d987241a8f2b56c0f870b299fabd
Cobse <Fredrik.jacobsen85@gmail.com>
2017-12-29 20:26:53 +0100

    Initial program
    Initial program

---
+++ Generate_10tabFile/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Generate_10tabFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileDirInput = new System.Windows.Forms.TextBox();
+            this.importFileButton = new System.Windows.Forms.Button();
+            this.exportFileButton = new System.Windows.Forms.Button();
+            this.fileDirOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // fileDirInput
+            // 
+            this.fileDirInput.Location = new System.Drawing.Point(139, 40);
+            this.fileDirInput.Name = "fileDirInput";
+            this.fileDirInput.Size = new System.Drawing.Size(242, 20);
+            this.fileDirInput.TabIndex = 0;
+            // 
+            // importFileButton
+            // 
+            this.importFileButton.Location = new System.Drawing.Point(42, 40);
+            this.importFileButton.Name = "importFileButton";
+            this.importFileButton.Size = new System.Drawing.Size(75, 23);
+            this.importFileButton.TabIndex = 1;
+            this.importFileButton.Text = "Hent fil";
+            this.importFileButton.UseVisualStyleBackColor = true;
+            this.importFileButton.Click += new System.EventHandler(this.ImportFileButton_Click);
+            // 
+            // exportFileButton
+            // 
+            this.exportFileButton.Location = new System.Drawing.Point(42, 98);
+            this.exportFileButton.Name = "exportFileButton";
+            this.exportFileButton.Size = new System.Drawing.Size(75, 23);
+            this.exportFileButton.TabIndex = 1;
+            this.exportFileButton.Text = "Eksporter fil";
+            this.exportFileButton.UseVisualStyleBackColor = true;
+            this.exportFileButton.Click += new System.EventHandler(this.ExportFileButton_Click);
+            // 
+            // fileDirOutput
+            // 
+            this.fileDirOutput.Location = new System.Drawing.Point(139, 98);
+            this.fileDirOutput.Name = "fileDirOutput";
+            this.fileDirOutput.Size = new System.Drawing.Size(242, 20);
+            this.fileDirOutput.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 176);
+            this.Controls.Add(this.exportFileButton);
+            this.Controls.Add(this.importFileButton);
+            this.Controls.Add(this.fileDirOutput);
+            this.Controls.Add(this.fileDirInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fileDirInput;
+        private System.Windows.Forms.Button importFileButton;
+        private System.Windows.Forms.Button exportFileButton;
+        private System.Windows.Forms.TextBox fileDirOutput;
+    }
+}
+
---
+++ Generate_10tabFile/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Generate_10tabFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string inputDir;
+        string fileName;
+        string fileNameExtension;
+
+        OpenFileDialog browseButton = new OpenFileDialog();
+        private void ImportFileButton_Click(object sender, EventArgs e)
+        {
+            browseButton.Filter = "Text files |*.tit";
+            if (browseButton.ShowDialog() == DialogResult.OK)
+            {
+                inputDir = Path.GetDirectoryName(browseButton.FileName);
+                fileName = Path.GetFileNameWithoutExtension(browseButton.SafeFileName);
+                fileNameExtension = Path.GetExtension(browseButton.SafeFileName);
+
+                fileDirInput.Text = browseButton.FileName;
+                fileDirOutput.Text = inputDir + "\\" + fileName+"(2)" + fileNameExtension;
+            }
+         }
+
+
+        List<string> newFileText = new List<string>();
+        string tmpString;
+
+        private void ExportFileButton_Click(object sender, EventArgs e)
+        {
+
+            
+            if (fileDirOutput.Text != "")
+            {
+                
+            foreach (string line in File.ReadLines(browseButton.FileName))
+            {
+                    //MessageBox.Show(line);
+                    
+                    tmpString = line.Insert(24, " ");
+                    tmpString = tmpString.Insert(2, " ");
+                    newFileText.Add(tmpString);
+            }
+            
+                //READ FILE AND DO SOMETHING
+                File.WriteAllLines(fileDirOutput.Text, newFileText.ToArray());
+                Application.Exit();
+            }
+            
+        }
+    }
+}
